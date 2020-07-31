70e8365457bcc2441bca088d49336eea0760ad98
naominext <naomigoddard@gmail.com>
2020-07-31 12:29:19 +0100

    Logo removed, added Feet text

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>App will go here</h1>
+        <h1>FEET App will go here</h1>
       </div>
     )
   }
