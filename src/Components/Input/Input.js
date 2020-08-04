e87227f2749a04220c747792d72ea59c478e17f7
naominext <naomigoddard@gmail.com>
2020-08-04 11:57:50 +0100

    Added content to Input.js

--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -1,3 +1,12 @@
 import React from 'react';
 import './Input.css';
 
+class Input extends React.Component {
+    render() {
+        return (
+            <input type="text" id="movie1" />
+        )
+    }
+}
+
+export default Input;
