c5e880821ef5a57970af9dddc07472a965990d7a
naominext <naomigoddard@gmail.com>
2020-08-07 12:09:20 +0100

    updated Form

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -6,10 +6,20 @@ class Form extends React.Component {
     constructor(props) {
         super(props);
         this.handleAddInput = this.handleAddInput.bind(this);
+        this.handleRemoveInput = this.handleRemoveInput.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleAddInput() {
         this.props.onAddInput();
+    }
+
+    handleRemoveInput() {
+        this.props.onRemoveInput();
+    }
+
+    handleSubmit() {
+        this.props.onSubmit();
     }
 
     render() {
@@ -22,7 +32,11 @@ class Form extends React.Component {
                         )
                     })
                 }
-                <a href="#" onClick={this.handleAddInput} >Add input</a>
+                <div className="add-remove">
+                    <a href="#" onClick={this.handleAddInput} >Add item</a>
+                    <a style={{ display: this.props.inputs.length > 2 ? 'inline' : 'none' }} href="#" onClick={this.handleRemoveInput} >Remove item</a>
+                </div>
+                <a id="submit" href="#" onClick={this.handleSubmit}>Decide for me</a>
             </form>
         )
     }
