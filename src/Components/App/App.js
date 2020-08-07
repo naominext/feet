d9e1485956bc0c84b3c6f3443a51d479c7f9ede5
naominext <naomigoddard@gmail.com>
2020-08-07 12:10:55 +0100

    update App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -17,14 +17,16 @@ class App extends React.Component {
           value: '',
         }
       ],
+      winner: {},
     };
 
     this.submitForm = this.submitForm.bind(this);
     this.updateInput = this.updateInput.bind(this);
     this.addInput = this.addInput.bind(this);
+    this.removeInput = this.removeInput.bind(this);
   }
 
-  addInput () {
+  addInput() {
     let inputs = this.state.inputs;
     let nextId = inputs.length + 1;
     inputs.push({
@@ -36,7 +38,17 @@ class App extends React.Component {
     })
   }
 
-  updateInput (id, newValue) {
+  removeInput() {
+    let inputs = this.state.inputs;
+    if (inputs.length > 2) {
+      inputs.pop();
+      this.setState({
+      inputs: inputs,
+    })
+    }
+  }
+
+  updateInput(id, newValue) {
     let inputs = this.state.inputs;
     let index = id - 1;
     inputs[index].value = newValue;
@@ -46,14 +58,25 @@ class App extends React.Component {
   }
 
   submitForm() {
-
+    let inputs = this.state.inputs;
+    let winner = Math.floor(Math.random() * inputs.length);
+    console.log(winner);
+    this.setState({
+      formSubmitted: true,
+      winner: inputs[winner],
+    })
   }
 
   render() {
     return (
       <div className="App">
+<<<<<<< HEAD
         <h1>This will be a heading</h1>
         < Form inputs={this.state.inputs} onInputChange={this.updateInput} onAddInput={this.addInput} />
+=======
+        <h1>FEET Heading will go here</h1>
+        < Form inputs={this.state.inputs} onInputChange={this.updateInput} onAddInput={this.addInput} onRemoveInput={this.removeInput} onSubmit={this.submitForm} />
+>>>>>>> bc8508a9dbc014f82ec4206c16086f8fa925e250
       </div>
     )
   }
