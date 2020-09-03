6258ac024e14f7d3c3cdf4c760b04df982bfddc4
Gideon Goddard <goddard.gcw@gmail.com>
2020-09-03 20:29:03 +0100

    Updates

---
+++ precache-manifest.8371e8f2773d7f2fac76612a40883420.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fb4b6f1a69a77a742108e1693c02fe6",
+    "url": "/feet/index.html"
+  },
+  {
+    "revision": "8834eed16dc08c61593c",
+    "url": "/feet/static/css/main.99a503a8.chunk.css"
+  },
+  {
+    "revision": "2ec2ab60386e7832c6eb",
+    "url": "/feet/static/js/2.0b1ee8cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/feet/static/js/2.0b1ee8cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8834eed16dc08c61593c",
+    "url": "/feet/static/js/main.577a644b.chunk.js"
+  },
+  {
+    "revision": "c818c96585d7d45ddaab",
+    "url": "/feet/static/js/runtime-main.3a984282.js"
+  }
+]);
